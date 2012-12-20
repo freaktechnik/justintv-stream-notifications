028347582e32a14a8dbecb17f40643f2773e4d61
freaktechnik <martin.a.giger@sunrise.ch>
2012-12-20 21:28:24 +0100

    update to 2.2.2 and a bit of additional fixing
    Changelog for 2.2.2:
    - better panel
    - better styling
    - channel image shown in list
    - pannel fits to its content
    - different message when no channel is live
    - better url handling
    
    Other fixes:
    - not showing notification when tab is active even when it wasn't when
    the request was sent

--- data/update-panel.js
+++ data/update-panel.js
@@ -1,18 +1,47 @@
 addon.port.on("add", function (channel) {
     var element = document.createElement('li');
     var link = document.createElement('a');
+	var image = new Image();
+	image.src = channel.image;
 	var textNode = document.createTextNode (channel.names[0]);
     element.id = channel.names[0];
+	link.appendChild(image);
 	link.appendChild(textNode);
     link.href = 'javascript:openTab("http://'+channel.url[0]+'")';
     element.appendChild(link);
     document.getElementById('channels').appendChild(element);
+	updatePanel();
 });
 
 addon.port.on("remove", function(channel) {
     document.getElementById('channels').removeChild(document.getElementById(channel)); 
+	updatePanel();
 });
 
 function openTab(url) {
     addon.port.emit("openTab",url);
 }
+
+function resizePanel() {
+	addon.port.emit("resizePanel",[212,document.body.scrollHeight]);
+}
+
+function showMessage() {
+	if(document.getElementById('channels').getElementsByTagName("LI").length>0&&document.getElementById("channelslive").style.display=='none') {
+		document.getElementById("channelslive").style.display = 'block';
+		document.getElementById("channels").style.display = 'block';
+		document.getElementById("channelsoffline").style.display = 'none';
+	}
+	else if(document.getElementById('channels').getElementsByTagName("LI").length==0&&document.getElementById("channelsoffline").style.display=='none') {
+		document.getElementById("channelslive").style.display = 'none';
+		document.getElementById("channels").style.display = 'none';
+		document.getElementById("channelsoffline").style.display = 'block';
+	}
+}
+
+function updatePanel() {
+	showMessage();
+	resizePanel();
+}
+
+window.onload = resizePanel;
