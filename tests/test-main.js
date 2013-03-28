7625277d95e3dbd00add01a0b83c8a00fa144792
freaktechnik <martin.a.giger@sunrise.ch>
2013-03-28 21:59:41 +0100

    Merge response error tests into one function

--- tests/test-main.js
+++ tests/test-main.js
@@ -1,24 +1,13 @@
 var main = require('./main');
-var { Channel, ChannelList, Sources, Source } = require('./channellist');
 
-exports['test 403 Error'] = function(test) {
+exports['test checkResponse'] = function(test) {
     test.assertEqual(main.checkResponse({'status':403}),0,"403 Errors are detected correctly");
-};
-exports['test 500 Error'] = function(test) {
     test.assertEqual(main.checkResponse({'status':500}),2,"500 Errors are detected correctly");
-};
-exports['test json Error'] = function(test) {
+    test.assertEqual(main.checkResponse({'status':0}),0,"Offline is detected correctly");
     test.assertEqual(main.checkResponse({'status':200,'json':{'error':"couldn't find user"}}),0,"json user Errors are detected correctly");
     test.assertEqual(main.checkResponse({'status':200,'json':{'error':"couldn't find channel"}}),0,"json channel Errors are detected correctly");
     test.assertEqual(main.checkResponse({'status':200,'json':{'error':"a"}}),2,"other json Errors are detected correctly");
-};
-exports['test offline'] = function(test) {
-    test.assertEqual(main.checkResponse({'status':0}),0,"Offline is detected correctly");
-};
-exports['test ratelimit'] = function(test) {
     test.assertEqual(main.checkResponse({'status':200,'json':{'rate_limited':true}}),2,"Ratelimit is detected correctly");
-};
-exports['test normal response'] = function(test) {
     test.assertEqual(main.checkResponse({'status':200,'json':{}}),1,"correct responses are detected correctly");
 };
 
