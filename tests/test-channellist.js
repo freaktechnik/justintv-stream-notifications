bb8b4c560dcb915ec76a4d81b3c14f164adbe547
freaktechnik <martin.a.giger@sunrise.ch>
2013-06-22 22:06:44 +0200

    Fix length on restore
    Also fix up a few tests and add more tests

--- tests/test-channellist.js
+++ tests/test-channellist.js
@@ -1,9 +1,10 @@
 var { Channel, ChannelList, Sources, Source, UpdateQueue } = require('./channellist');
+var baseprovider = require('./baseprovider');
 
 exports['test channel url comparison'] = function(test) {
     var channel = getFullChannel();
-    test.assert(channel.compareUrl('http://foo.bar'));
-    test.assert(!channel.compareUrl('http://foo.bar/archive'));
+    test.assert(channel.compareUrl('http://foo.bar/lorem'));
+    test.assert(channel.compareUrl('http://foo.bar/lorem/archive'));
     test.assert(!channel.compareUrl('http://asdf.com'));
 };
 
@@ -16,35 +17,51 @@ exports['test first source object'] = function(test) {
 };
 
 function getFullChannel() {
-    return new Channel(Channel.FULL,new Source(Source.TYPE_USER,Source.TYPE_USER,'justintv'),{'channel_url':'http://foo.bar','title':'lorem ipsum','login':'test','image_url_tiny':'http://foo.bar/0.jpg',
-                        'image_url_medium':'http://foo.bar/1.jpg','channel_background_color':'#ffffff','channel_text_color':'#000000','channel_link_color':'#cccccc'});
+    return new Channel(Channel.FULL,new Source(Source.TYPE_USER,Source.TYPE_USER,'justintv'),{'url':'http://foo.bar/lorem','name':'lorem ipsum','login':'test','panelAvatar':'http://foo.bar/0.jpg',
+                        'notificationAvatar':'http://foo.bar/1.jpg','backgroundColor':'#ffffff','textColor':'#000000','linkColor':'#cccccc'});
 }
 
 exports['test ChannelList enumeration'] = function(test) {
     test.waitUntilDone();
     var channels = createDummyChannelList().channels;
-    var ok = true;
-    for(var ch in channels) {
-        test.assertEqual(channels[ch] instanceof Channel,true,"ChannelList.channels property is a Channel");
+    for(var type in channels) {
+        for(var ch in channels[type]) {
+            test.assertEqual(channels[type][ch] instanceof Channel,true,"ChannelList.channels property is a Channel");
+        }
     }
     test.done();
 };
 
 exports['test ChannelList length property'] = function(test) {
     test.waitUntilDone();
-    var channels = createDummyChannelList().channels;
+    var channellist = createDummyChannelList();
     test.assertEqual(new ChannelList().channels.length,0,"ChannelList.channels length is initialized correctly");
-    test.assertEqual(channels.propertyIsEnumerable('length'),false,"ChannelList.channels length property isn't enumerable");
-    test.assertEqual(channels.length,2,"ChannelList.channels length gets extended correctly");
+    test.assertEqual(channellist.channels.propertyIsEnumerable('length'),false,"ChannelList.channels length property isn't enumerable");
+    test.assertEqual(channellist.channels.length,2,"ChannelList.channels length gets extended correctly");
+
+    channellist.initLength(5);
+    test.assertEqual(channellist.channels.length,2,"ChannelList.channels.length gets set correctly when already existing");
+    
+    channellist.channels.length = 0;
+    channellist.initLength(7);
+    test.assertEqual(channellist.channels.length,7,"ChannelList.channels.length gets set correctly when null");
+    
     test.done();
 };
 
 function createDummyChannelList() {
     var list = new ChannelList();
-    list.addChannel({'login':'someChannel','channel_url':'http://twitch.tv/freaktechnik','title':'just a channel','image_url_tiny':'','image_url_medium':''},
+    var ch1 = baseprovider.channelInfo;
+    var ch2 = baseprovider.channelInfo;
+    ch1.login = 'someChannel';
+    ch2.login = 'yetAnotherChannel';
+    ch1.type = 'justintv';
+    ch2.type = 'twitch';
+    
+    list.addChannel(ch1,
                     new Source(Source.TYPE_USERNAME,'someChannel','justintv'),true);
-    list.addChannel({'login':'yetAnotherChannel','channel_url':'http://twitch.tv/freaktechnik','title':'just another channel','image_url_tiny':'','image_url_medium':''},
-                    new Source(Source.TYPE_USERNAME,'yetAnotherChannel','justintv'),true);
+    list.addChannel(ch2,
+                    new Source(Source.TYPE_USERNAME,'yetAnotherChannel','twitch'),true);
     return list;
 }
 
--- tests/test-twitch.js
+++ tests/test-twitch.js
@@ -1,21 +1,11 @@
-var justintv = require('./justintv');
+var justintv = require('./twitch');
 var baseprovider = require('./baseprovider');
 
 exports['test checkResponse'] = function(test) {
     test.assertEqual(justintv.checkResponse({'status':403}),0,"403 Errors are detected correctly");
     test.assertEqual(justintv.checkResponse({'status':500}),2,"500 Errors are detected correctly");
     test.assertEqual(justintv.checkResponse({'status':0}),0,"Offline is detected correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'error':"couldn't find user"}}),0,"json user Errors are detected correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'error':"couldn't find channel"}}),0,"json channel Errors are detected correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'error':"a"}}),2,"other json Errors are detected correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'rate_limited':true,'message':'127.0.0.1 is ratelimitted'}}),2,"Ratelimit is detected correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'rate_limited':true,'message':'199.9.248.0 is ratelimitted'}}),1,"Justin.tv API bug is ignored correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'rate_limited':true,'message':'199.9.248.255 is ratelimitted'}}),1,"Justin.tv API bug is ignored correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'rate_limited':true,'message':'199.9.249.0 is ratelimitted'}}),1,"Justin.tv API bug is ignored correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'rate_limited':true,'message':'199.9.251.0 is ratelimitted'}}),1,"Justin.tv API bug is ignored correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'rate_limited':true,'message':'199.9.255.255 is ratelimitted'}}),1,"Justin.tv API bug is ignored correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'rate_limited':false,'message':'199.9.255.255 is not ratelimitted'}}),1,"Ratelimit is handled correctly");
-    test.assertEqual(justintv.checkResponse({'status':200,'json':{'rate_limited':false,'message':'127.0.0.1 is not ratelimitted'}}),1,"Ratelimit is handled correctly");
+    test.assertEqual(justintv.checkResponse({'status':200,'json':{'error':"a"}}),2,"json Errors are detected correctly");
     test.assertEqual(justintv.checkResponse({'status':200,'json':{}}),1,"correct responses are detected correctly");
 };
 
@@ -49,7 +39,7 @@ exports['test basic unified channel info'] = function(test) {
 
 exports['test favorites constructor'] = function(test) {
     test.waitUntilDone();
-    var reqObj = justintv.getUserFavorites();
+    var reqObj = justintv.getUserFavorites({'name':'lorem','page':0});
     test.assertEqual(typeof(reqObj.url),'string');
     test.assert(typeof reqObj.headers == "object" || !reqObj.headers);
     test.assert(typeof reqObj.content == "string" || typeof reqObj.content == "object" || !reqObj.content);
@@ -60,7 +50,7 @@ exports['test favorites constructor'] = function(test) {
 
 exports['test detail constructor'] = function(test) {
     test.waitUntilDone();
-    var reqObj = justintv.getChannelDetails();
+    var reqObj = justintv.getChannelDetails({'name':'ipsum'});
     test.assertEqual(typeof(reqObj.url),'string');
     test.assert(typeof reqObj.headers == "object" || !reqObj.headers);
     test.assert(typeof reqObj.content == "string" || typeof reqObj.content == "object" || !reqObj.content);
