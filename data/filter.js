618c16a78d39a7514564d0a3d407ad67f1dce2c3
Martin Giger <martin.a.giger@sunrise.ch>
2015-01-01 16:44:44 +0100

    Ignore case when filtering

--- data/filter.js
+++ data/filter.js
@@ -8,13 +8,15 @@
  
 function filter(query, root, rules) {
     var node;
+    if(query)
+        query = query.toLowerCase();
 
     rules.forEach(function(rule) {
         rule.attribute = rule.attribute || "textContent";
         node = root.querySelectorAll(rule.target);
         for(var i = 0; i < node.length; ++i) {
             if(query) {
-                if(node[i].querySelector(rule.subtarget)[rule.attribute].indexOf(query) != -1)
+                if(node[i].querySelector(rule.subtarget)[rule.attribute].toLowerCase().indexOf(query) != -1)
                     show(node[i]);
                 else
                     hide(node[i]);
