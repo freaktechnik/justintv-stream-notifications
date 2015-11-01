26acbeb225748ec762c67961c3427e6808f8f42c
Martin Giger <martin@humanoids.be>
2015-11-01 23:26:30 +0100

    Add tests for livestreamer

---
+++ test/test-livestreamer.js
@@ -0,0 +1,45 @@
+/**
+ * Livestreamer module tests
+ * @author Martin Giger
+ * @license MPL-2.0
+ */
+"use strict";
+
+const livestreamer = require("../lib/livestreamer");
+const { prefs } = require("sdk/simple-prefs");
+const { when } = require("sdk/event/utils");
+
+exports["test existance event"] = function*(assert) {
+    let p = when(livestreamer.events, "existance");
+    prefs.livestreamer_enabled = true;
+    yield p;
+    p = when(livestreamer.events, "existance");
+    prefs.livestreamer_enabled = false;
+    yield p;
+    p = when(livestreamer.events, "existance");
+    prefs.livestreamer_path = "/does/not/exist";
+    yield p;
+    prefs.livestreamer_path = "";
+};
+
+exports["test show property"] = (assert) => {
+    prefs.livestreamer_enabled = true;
+    assert.ok(!livestreamer.show);
+    prefs.livestreamer_enabled = false;
+    // Can't test for this, since we don't require a livestreamer for unit tests.
+};
+
+exports["test default property"] = (assert) => {
+    assert.equal(livestreamer.default, prefs.livestreamer_enabled);
+    prefs.livestreamer_enabled = true;
+    assert.equal(livestreamer.default, prefs.livestreamer_enabled);
+    prefs.livestreamer_enabled = false;
+    assert.equal(livestreamer.default, prefs.livestreamer_enabled);
+};
+
+exports["test event object interface"] = (assert) => {
+    assert.equal(typeof livestreamer.events, "object");
+    assert.equal(typeof livestreamer.events.on, "function");
+};
+
+require("sdk/test").run(exports);
