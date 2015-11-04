b3b90d9cfeced98a05b40ad7a110ff9b10eb113f
Martin Giger <martin@humanoids.be>
2015-11-04 11:47:25 +0100

    Make tests fail again

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,10 +14,7 @@ module.exports = function(grunt) {
             ' * obtain one at https://mozilla.org/MPL/2.0/.\n */\n',
         shell: {
             jpmTest: {
-                command: 'jpm test --tbpl -b <%= firefoxBinary %>',
-                options: {
-                    failOnError: false
-                }
+                command: 'jpm test --tbpl -b <%= firefoxBinary %>'
             }
         },
         jshint: {
