dff6eeb57dd961e39b0ebcfe3c6c1e9cc0a484cd
Martin Giger <martin@humanoids.be>
2015-11-09 02:39:13 +0100

    Move the click callback out of the notifier and fix its tests

--- test/test-notifier.js
+++ test/test-notifier.js
@@ -9,20 +9,20 @@ const { Notifier } = requireHelper('../lib/notifier'),
       { Channel } = requireHelper('../lib/channel/core');
 const { getChannel } = require("./channeluser/utils");
 const { prefs} = require("sdk/simple-prefs");
+const { cleanUI } = require("sdk/test/utils");
 
 exports.testNotifierPrefs = function(assert) {
-    let notifier = new Notifier();
+    let notifier = new Notifier({ onClick: function() {} });
     assert.equal(notifier.onlineNotifications, prefs.onlineNotification);
     assert.equal(notifier.titleNotifications, prefs.titleChangeNotification);
     assert.equal(notifier.offlineNotifications, prefs.offlineNotification);
 };
 
-//TODO check if notifications are sent (observer?)
 exports.testNotifier = function(assert) {
     let oldVal = prefs.offlineNotification;
     prefs.offlineNotification = true;
 
-    let notifier = new Notifier();
+    let notifier = new Notifier({ onClick: function() {} });
     var channel = getChannel();
     channel.live = true;
     channel.id = 1;
@@ -30,6 +30,7 @@ exports.testNotifier = function(assert) {
     assert.ok(!notifier.channelTitles.has(channel.id));
 
     notifier.sendNotification(channel);
+
     assert.ok(notifier.channelTitles.has(channel.id));
     assert.equal(notifier.channelTitles.get(channel.id), channel.title);
     notifier.sendNotification(channel);
@@ -51,6 +52,9 @@ exports.testNotifier = function(assert) {
     assert.equal(notifier.channelTitles.length, mapLength);
 
     prefs.offlineNotification = oldVal;
+
+    // close all notifications that are still open (on systems without native notifications)
+    cleanUI();
 };
 
 require("sdk/test").run(exports);
