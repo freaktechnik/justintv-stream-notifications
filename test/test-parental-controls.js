38117be1705445b3aa68879c6965e01568562985
Martin Giger <martin@humanoids.be>
2016-01-30 02:50:58 +0100

    Tests and things, mostly fixing, some pc stuff too

---
+++ test/test-parental-controls.js
@@ -0,0 +1,25 @@
+/**
+ * @author Martin Giger
+ * @license MPL-2.0
+ */
+"use strict";
+
+const requireHelper = require("./require_helper");
+const ParentalControls = requireHelper("../lib/parental-controls");
+const { CC } = require("chrome");
+const NativeParentalControls = CC("@mozilla.org/parental-controls-service;1", "nsIParentalControlsService");
+
+exports.testPCEnabled = function(assert) {
+    assert.equal(ParentalControls.enabled, NativeParentalControls.parentalControlsEnabled, "Parental controls state is correct");
+};
+
+exports.testCanBrowseUrl = function(assert) {
+    assert.ok(ParentalControls.canBrowse("http://example.com"));
+};
+
+exports.testLogDoesntThrow = function(assert) {
+    ParentalControls.log("https://localhost");
+    assert.pass("Log didn't throw");
+};
+
+require("sdk/test").run(exports);
