cf43f464640d6c9018cd5a8678b25e29c83f4ffb
Martin Giger <martin@humanoids.be>
2016-03-01 16:47:49 +0100

    Don't need those either

--- test/test-parental-controls.js
+++ test/test-parental-controls.js
@@ -8,19 +8,17 @@ const requireHelper = require("./require_helper");
 const { Ci } = require("chrome");
 const { factoryByContract } = require("sdk/platform/xpcom");
 const NativeParentalControls = factoryByContract("@mozilla.org/parental-controls-service;1").createInstance(Ci.nsIParentalControlsService);
+const ParentalControls = requireHelper("../lib/parental-controls");
 
 exports.testPCEnabled = function(assert) {
-    const ParentalControls = requireHelper("../lib/parental-controls");
     assert.equal(ParentalControls.enabled, NativeParentalControls.parentalControlsEnabled, "Parental controls state is correct");
 };
 
 exports.testCanBrowseUrl = function(assert) {
-    const ParentalControls = requireHelper("../lib/parental-controls");
     assert.ok(ParentalControls.canBrowse("http://example.com"));
 };
 
 exports.testLogDoesntThrow = function(assert) {
-    const ParentalControls = requireHelper("../lib/parental-controls");
     ParentalControls.log("https://localhost");
     assert.pass("Log didn't throw");
 };
