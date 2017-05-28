bb50981f249e7297eab392e8d62a1e743f8e6719
Martin Giger <martin@humanoids.be>
2017-05-28 21:30:49 +0200

    Set the language of a document

--- src/content/l10n.js
+++ src/content/l10n.js
@@ -36,6 +36,8 @@ function translateElementAttributes(element) {
 }
 
 function translateElement(element = document) {
+    // Set the language attribute of the document.
+    document.querySelector("html").setAttribute("lang", browser.i18n.getUILanguage().substr(0, 2));
     //TODO follow the tranlsate attribute's instructions (yes/no/inherit)
     // Get all children that are marked as being translateable.
     const children = element.querySelectorAll('*[data-l10n-id]');
