157874436edeb76c4d9bc6e0ce90a3f06766082e
Martin Giger <martin@humanoids.be>
2017-09-19 15:59:13 +0200

    Fix search in explore tab behaving weirdly when having an empty query and explore tab reloading when clicking on the tab

--- src/list/middleware.js
+++ src/list/middleware.js
@@ -28,28 +28,38 @@ export default (port) => ({ getState, dispatch }) => (next) => (action) => {
         port.send(action.command, action.payload);
     }
     // State changes that trigger a backend command
-    else if(action.type === "setTab" && action.payload === 3) {
-        if(!state.ui.search || !state.ui.query.length) {
-            port.send("explore", state.ui.currentProvider);
+    else if(action.type === "setTab") {
+        if(state.ui.tab === action.payload) {
+            return;
         }
-        else {
-            port.send("search", {
-                type: state.ui.currentProvider,
-                query: state.ui.query
-            });
+        else if(action.payload === 3) {
+            if(!state.ui.search || !state.ui.query.length) {
+                port.send("explore", state.ui.currentProvider);
+            }
+            else {
+                port.send("search", {
+                    type: state.ui.currentProvider,
+                    query: state.ui.query
+                });
+            }
         }
     }
-    else if(action.type === "search" && action.payload.length && state.ui.tab === 3) {
-        port.send("search", {
-            type: state.ui.currentProvider,
-            query: action.payload
-        });
+    else if(action.type === "search" && state.ui.tab === 3) {
+        if(action.payload.length) {
+            port.send("search", {
+                type: state.ui.currentProvider,
+                query: action.payload
+            });
+        }
+        else {
+            port.send("explore", state.ui.currentProvider);
+        }
         dispatch({
             type: "loading"
         });
     }
     else if(action.type === "setFeatured") {
-        if((state.ui.search && state.ui.query != action.payload.q) || state.ui.currentProvider != action.payload.type) {
+        if((state.ui.search && ((state.ui.query.length && state.ui.query != action.payload.q) || !("q" in action.payload))) || state.ui.currentProvider != action.payload.type) {
             return;
         }
         action = {
@@ -57,7 +67,7 @@ export default (port) => ({ getState, dispatch }) => (next) => (action) => {
             payload: action.payload.channels
         };
     }
-    else if(action.type === "toggleSearch" && state.ui.search && state.ui.query.length) {
+    else if(action.type === "toggleSearch" && state.ui.search && state.ui.query.length && state.ui.tab === 3) {
         port.send("explore", state.ui.currentProvider);
         dispatch({
             type: "loading"
