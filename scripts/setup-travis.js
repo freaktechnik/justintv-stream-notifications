23e96fe83b335d1a90ea9e0be14f93870343400c
Martin Giger <martin@humanoids.be>
2017-10-09 01:06:55 +0200

    remove - because docs don't have one

--- scripts/setup-travis.js
+++ scripts/setup-travis.js
@@ -12,7 +12,7 @@ username = ${process.env.TRANSIFEX_USER}
 password = ${process.env.TRANSIFEX_PASSWORD}
 token =`;
             return writefile(path.join(__dirname, '../.transifexrc'), fileContent, {
-                encoding: 'utf-8'
+                encoding: 'utf8'
             });
         }
         return Promise.resolve();
