8dd814782532ccaebe938ccfaa1c88488d2c75e2
Martin Giger <martin@humanoids.be>
2017-10-09 01:05:38 +0200

    Write txrc on ci using node script

---
+++ scripts/setup-travis.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const fs = require("fs"),
+    { promisify } = require("util"),
+    path = require("path"),
+    writefile = promisify(fs.writeFile),
+    writeTXRCFromEnv = () => {
+        if(process.env.TRANSIFEX_USER && process.env.TRANSIFEX_PASSWORD) {
+            const fileContent = `[https://transifex.com]
+hostname = https://www.transifex.com
+username = ${process.env.TRANSIFEX_USER}
+password = ${process.env.TRANSIFEX_PASSWORD}
+token =`;
+            return writefile(path.join(__dirname, '../.transifexrc'), fileContent, {
+                encoding: 'utf-8'
+            });
+        }
+        return Promise.resolve();
+    };
+
+writeTXRCFromEnv()
+    .catch(() => {
+        // discard errors
+    });
