09e0e567eed8543796c3a13e786930a4c2c87ddb
Martin Giger <martin@humanoids.be>
2017-11-05 21:57:14 +0100

    refactor: Make TabStrip a simple component

--- src/list/components/toolbar/tab-strip.jsx
+++ src/list/components/toolbar/tab-strip.jsx
@@ -6,36 +6,28 @@ import { LIVE_TAB, NONLIVE_TAB, OFFLINE_TAB, EXPLORE_TAB } from '../../constants
 
 //TODO space or enter should focus tab panel.
 
-class TabStrip extends NavigateableList {
-    static get defaultProps() {
-        return {
-            active: LIVE_TAB,
-            showNonlive: false
-        };
+const TabStrip = (props) => {
+    const nonlive = [];
+    if(props.showNonlive) {
+        nonlive.push( <Tab title="panel_tab_nonlive" onClick={ () => props.onTabSelect(NONLIVE_TAB) } active={ props.active === NONLIVE_TAB } key={ NONLIVE_TAB }/> );
     }
-
-    static get propTypes() {
-        return {
-            active: PropTypes.number,
-            showNonlive: PropTypes.bool,
-            onTabSelect: PropTypes.func.isRequired
-        };
-    }
-
-    render() {
-        const children = [ <Tab title="panel_tab_live" onClick={ () => this.props.onTabSelect(LIVE_TAB) } active={ this.props.active === LIVE_TAB } key={ LIVE_TAB }/> ];
-        if(this.props.showNonlive) {
-            children.push(<Tab title="panel_tab_nonlive" onClick={ () => this.props.onTabSelect(NONLIVE_TAB) } active={ this.props.active === NONLIVE_TAB } key={ NONLIVE_TAB }/>);
-        }
-        children.push(<Tab title="panel_tab_offline" onClick={ () => this.props.onTabSelect(OFFLINE_TAB) } active={ this.props.active === OFFLINE_TAB } key={ OFFLINE_TAB }/>);
-        children.push(<Tab title="panel_tab_explore" onClick={ () => this.props.onTabSelect(EXPLORE_TAB) } active={ this.props.active === EXPLORE_TAB } key={ EXPLORE_TAB }/>);
-
-        const element = super.render();
-        return React.cloneElement(element, {
-            className: "tabstrip inline-list",
-            role: "tablist"
-        }, ...this.mapChildren(children));
-    }
-}
+    return (
+        <NavigateableList className="tabstrip inline-list" role="tablist">
+            <Tab title="panel_tab_live" onClick={ () => props.onTabSelect(LIVE_TAB) } active={ props.active === LIVE_TAB } key={ LIVE_TAB }/>
+            { nonlive }
+            <Tab title="panel_tab_offline" onClick={ () => props.onTabSelect(OFFLINE_TAB) } active={ props.active === OFFLINE_TAB } key={ OFFLINE_TAB }/>
+            <Tab title="panel_tab_explore" onClick={ () => props.onTabSelect(EXPLORE_TAB) } active={ props.active === EXPLORE_TAB } key={ EXPLORE_TAB }/>
+        </NavigateableList>
+    );
+};
+TabStrip.defaultProps = {
+    active: LIVE_TAB,
+    showNonlive: false
+};
+TabStrip.propTypes = {
+    active: PropTypes.number,
+    showNonlive: PropTypes.bool,
+    onTabSelect: PropTypes.func.isRequired
+};
 
 export default TabStrip;
