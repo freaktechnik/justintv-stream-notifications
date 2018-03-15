4dbd4aad54fc6df08c2e9deb5f80a02eceba1f24
Martin Giger <martin@humanoids.be>
2018-03-15 17:49:37 +0100

    feat: Mark keyboard shortcuts in markup

--- src/list/components/toolbar/tools.jsx
+++ src/list/components/toolbar/tools.jsx
@@ -14,7 +14,7 @@ const Tools = (props) =>
             props.onToolClick("refresh");
         } }/>
         <Tool title="panel_search" icon="magnifying-glass" onClick={ () => props.onToolClick("toggleSearch") } active={ props.searching }/>
-        <Tool title="panel_refresh" icon="reload" onClick={ () => props.onToolClick("refresh") } className={ props.queuePaused ? "" : "loading" } onContextMenu={ props.onRefreshContextMenu }/>
+        <Tool title="panel_refresh" icon="reload" onClick={ () => props.onToolClick("refresh") } className={ props.queuePaused ? "" : "loading" } onContextMenu={ props.onRefreshContextMenu } aria-keyshortcuts="F5"/>
         <Tool title="panel_manage" icon="wrench" onClick={ () => props.onToolClick("configure") }/>
     </ul> );
 Tools.defaultProps = {
