c8bde3e0083916a947d76977d01fc09988283000
Martin Giger <martin@humanoids.be>
2018-07-30 02:09:11 +0200

    tests: test with properer translations

--- test/helpers/default-behavior.js
+++ test/helpers/default-behavior.js
@@ -1,11 +1,18 @@
 import manifest from '../../webextension/manifest.json';
+import messages from '../../_locales/en/messages.json';
 
 export const setup = () => {
     // Work around sinon-chrome being sinon < 2
     browser.storage.local.get.callsFake((props) => Promise.resolve(props));
     browser.storage.local.set.resolves();
-    browser.i18n.getUILanguage.returns("en_US");
-    browser.i18n.getMessage.returnsArg(0);
+    browser.i18n.getUILanguage.returns("en-US");
+    browser.i18n.getMessage.callsFake((string) => {
+        if(string in messages) {
+            return messages[string].message;
+        }
+        console.warn("no translation for", string);
+        return string;
+    });
     browser.runtime.getPlatformInfo.resolves({
         os: 'ava-test',
         arch: 'node',
