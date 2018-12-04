e8b7e15da6f7bf33cd22b02fe6ecda26d59ceb0d
Martin Giger <martin@humanoids.be>
2018-12-04 20:07:53 +0100

    wip: actions and rules and stuff

---
+++ src/background/actions/conditions/live-state.js
@@ -0,0 +1,15 @@
+import Condition from './condition.js';
+import LiveState from '../../live-state.json';
+
+export default class LiveStateCondition extends Condition {
+    static get options() {
+        return super.options.concat(Object.keys((LiveState).filter((k) => !k.startsWith('TOWARD_'))));
+    }
+
+    checkParam(context, param) {
+        if(param == 'any') {
+            return true;
+        }
+        return context.live.state === LiveState[param];
+    }
+}
