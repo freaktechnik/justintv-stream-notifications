9e0e45efb7327509df1cbe9c0451acf9f520f4e3
Martin Giger <martin@humanoids.be>
2019-01-05 15:54:26 +0100

    chore: fix the gh pages release template

--- scripts/build-website-changelog.js
+++ scripts/build-website-changelog.js
@@ -1,14 +1,17 @@
 "use strict";
 
-const conventionalChangelog = require("conventional-changelog"),
-    pkg = require("../package.json"),
-    stp = require("stream-to-promise"),
+const pkg = require("../package.json"),
     fs = require("fs").promises,
     path = require("path"),
     git = require("simple-git"),
 
     repo = git(path.resolve(__dirname, '..')),
     file = path.join(__dirname, '..', 'changes', `${pkg.version}.md`),
+    content = `---
+title: You've upgraded Live Stream Notifier
+version: v${pkg.version}
+permalink: /changes/${pkg.version}
+---`,
     promisify = (res, rej) => (err, result) => {
         if(err) {
             rej(err);
@@ -21,17 +24,7 @@ const conventionalChangelog = require("conventional-changelog"),
 new Promise((resolve, reject) => {
     repo.checkout('gh-pages', promisify(resolve, reject));
 })
-    .then(() => stp(conventionalChangelog({
-        preset: 'angular',
-        releaseCount: 2
-    }, null, null, null, {
-        headerPartial: `---
-  title: You've upgraded Live Stream Notifier
-  version: v${pkg.version}
-  permalink: /changes/${pkg.version}
----`
-    })))
-    .then((res) => fs.writeFile(file, res))
+    .then((res) => fs.writeFile(file, content, res))
     .then(() => new Promise((resolve, reject) => {
         repo.add(file, promisify(resolve, reject));
     }))
